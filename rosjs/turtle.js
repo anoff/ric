261c1316326db787951c2f099b233aa9011d7398
anoff <offenhaeuser@gmail.com>
2018-10-27 01:24:13 +0200

    :star2: feat: open/close tool

---
+++ rosjs/turtle.js
@@ -0,0 +1,44 @@
+const WebSocket = require('ws')
+const argv = require('minimist')(process.argv.slice(2))
+
+const ws = new WebSocket('ws://' + argv.ros_master + ':9090/')
+
+ws.on('open', function open () {
+  console.log('Connection open')
+  const subscribe = JSON.stringify({
+    'op': 'subscribe',
+    'topic': '/turtle1/pose'
+  })
+  ws.send(subscribe)
+})
+
+ws.on('message', function incoming (data) {
+  console.log(data)
+})
+
+function moveTurtle (ws, x) {
+  const cmd = JSON.stringify({
+    'op': 'publish',
+    'topic': '/turtle1/cmd_vel',
+    'msg': {
+      'linear': {
+        'x': x,
+        'y': 5.0,
+        'z': 5.0
+      },
+      'angular': {
+        'x': 5.0,
+        'y': 5.0,
+        'z': 5.0
+      }
+    }
+  })
+  ws.send(cmd)
+}
+
+let turtleX = 0
+
+setInterval(() => {
+  moveTurtle(ws, turtleX)
+  turtleX += 1
+}, 1000)
