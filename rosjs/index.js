0180251d63fe374895f8f4f9babd824383553c76
anoff <offenhaeuser@gmail.com>
2018-10-27 02:27:04 +0200

    :star2: feat: add logging and call handler

--- rosjs/index.js
+++ rosjs/index.js
@@ -1,33 +1,38 @@
-const argv = require('minimist')(process.argv.slice(2))
+/* global log */
+const pino = require('pino')
+const minimist = require('minimist')
 const BridgeHandler = require('./lib/bridgeHandler')
-const websocketUrl = 'ws://' + argv.ros_master + ':9090/'
 
+const argv = minimist(process.argv.slice(2))
+global.log = pino()
+const websocketUrl = 'ws://' + argv.ros_master + ':9090/'
 const ros = new BridgeHandler(websocketUrl)
 
 ros.ws.on('open', () => {
-  console.log('Connection open')
+  log.info('Connection open')
   init()
 })
 
 function init () {
+  /*
   ros.subscribe('/festo/cobotv1_1/festo_status', data => {
     if (data.msg.sequence % 10 === 0) console.log(JSON.stringify(data))
   })
+  */
+  let toolClosed = true
+  setInterval(() => {
+    closeTool(toolClosed)
+    toolClosed = !toolClosed
+  }, 1000)
 }
 
 function closeTool (closed) {
-  const cmd = JSON.stringify(
-    { 'op': 'call_service',
-      'service': '/festo/cobotv1_1/set_pressure',
-      'args': {
-        'required_pressure': {
-          'sequence': 0,
-          'p1': closed ? 1.0 : -1.0,
-          'p2': 0.0,
-          'weight': 0.1
-        }
-      }
-    })
-  ws.send(cmd)
-  console.log(cmd)
+  ros.call('/festo/cobotv1_1/set_pressure', {
+    'required_pressure': {
+      'sequence': 0,
+      'p1': closed ? 1.0 : -1.0,
+      'p2': 0.0,
+      'weight': 0.1
+    }
+  })
 }
